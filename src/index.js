fe47aab0cf8f7b4d89e05dc562154b7d4db72b31
Filip Š <filip.stamcar@hotmail.com>
2022-05-28 13:01:26 +0200

    Parcel 2.1.1

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// Taken from https://github.com/npm/node-semver/blob/main/internal/debug.js
+
+const debug = (
+  typeof process === 'object' &&
+  process.env &&
+  process.env.NODE_DEBUG &&
+  /\bsemver\b/i.test(process.env.NODE_DEBUG)
+) ? (...args) => console.error('SEMVER', ...args)
+  : () => {}
+
+debug('Hello World')
